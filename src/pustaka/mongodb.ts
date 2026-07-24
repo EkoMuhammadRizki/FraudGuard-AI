96f96889ba89a4013c9888e9c8c227df4d690979
Muhammad Muhibin <muhibinmuhammad@gmail.com>
2026-07-24 16:16:38 +0700

    Fix MongoDB Atlas connection string credentials to valid user fraudguard

--- src/pustaka/mongodb.ts
+++ src/pustaka/mongodb.ts
@@ -1,7 +1,7 @@
 import { MongoClient, Db } from "mongodb";
 
-const MONGO_URI = "mongodb://fraudguard:AmankanFraud123@clusterfraud-shard-00-00.cw2s6z.mongodb.net:27017,clusterfraud-shard-00-01.cw2s6z.mongodb.net:27017,clusterfraud-shard-00-02.cw2s6z.mongodb.net:27017/fraud_detection?ssl=true&replicaSet=atlas-c6o9bq-shard-0&authSource=admin&appName=ClusterFraud";
-const DB_NAME = "fraud_detection";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://fraudguard:amankan_fraud@clusterfraud.cw2s6z.mongodb.net/fraud_detection?retryWrites=true&w=majority";
+const DB_NAME = process.env.DB_NAME || "fraud_detection";
 
 let client: MongoClient | null = null;
 let db: Db | null = null;
@@ -16,3 +16,4 @@ export async function connectToDatabase(): Promise<{ client: MongoClient; db: Db
     db = client.db(DB_NAME);
     return { client, db };
 }
+
